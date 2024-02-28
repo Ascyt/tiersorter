8e5b2889b6a9ef5b58fd124d5f12f2074da029ee
Ascyt <filip.schauer07@gmail.com>
2024-02-28 20:49:59 +0100

    Added json upload and download buttons

--- src/app/main/initialize/initialize.component.ts
+++ src/app/main/initialize/initialize.component.ts
@@ -11,8 +11,10 @@ import { ValueInputComponent } from './value-input/value-input.component';
   styleUrl: './initialize.component.scss'
 })
 export class InitializeComponent {
-  public values: Value[] = this.valuesService.values;
   @Output() public startSorter = new EventEmitter();  
+  public get values(): Value[] {
+    return this.valuesService.values;
+  }
 
   constructor(private valuesService: ValuesService) { }
 
@@ -26,5 +28,41 @@ export class InitializeComponent {
 
   public onSubmit(): void {
     this.startSorter.emit();
+  }
+
+  downloadJson():void {
+    const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(this.values));
+    const downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute("href", dataStr);
+    downloadAnchorNode.setAttribute("download", "values.json");
+    document.body.appendChild(downloadAnchorNode); // required for firefox
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+  }
+
+  uploadJson(event: Event):void {
+    const input = event.target as HTMLInputElement;
+    const file = input.files?.[0];
+    if (file === undefined) 
+    {
+      console.error('No file selected');
+      return;
+    }
+    const reader = new FileReader();
+    reader.onload = () => {
+      const text = reader.result as string;
+      try {
+        const values = JSON.parse(text);
+        if (Array.isArray(values)) {
+          this.valuesService.values = values;
+        }
+        else {
+          console.error('Invalid JSON file');
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    reader.readAsText(file);
   }
 }
