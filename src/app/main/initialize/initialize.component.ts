b96f788fca3cc19ba2fe66f507a2b1dff52e0bae
Ascyt <filip.schauer07@gmail.com>
2024-02-29 09:53:08 +0100

    Increased width of item input + minor changes

--- src/app/main/initialize/initialize.component.ts
+++ src/app/main/initialize/initialize.component.ts
@@ -92,7 +92,7 @@ export class InitializeComponent {
     const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(this.values));
     const downloadAnchorNode = document.createElement('a');
     downloadAnchorNode.setAttribute("href", dataStr);
-    downloadAnchorNode.setAttribute("download", "values.json");
+    downloadAnchorNode.setAttribute("download", "tiersorter-items.json");
     document.body.appendChild(downloadAnchorNode); // required for firefox
     downloadAnchorNode.click();
     downloadAnchorNode.remove();
